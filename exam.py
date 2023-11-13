c2d9a0962a58b08d5cc043d3dee5ac0d9e8aef8b
ByungWook-Lee96 <byungwook414@gmail.com>
2023-11-13 20:40:30 +0900

    add logfile & fixed code

--- exam.py
+++ exam.py
@@ -1,42 +1,43 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait  # 로딩될 때까지 대기
 from selenium.webdriver.support import expected_conditions as EC  # 로딩될 때까지 대기
-from selenium.webdriver.common.keys import Keys  # 키보드 클릭할 때 쓸 부분
-from selenium.webdriver.chrome.options import Options
-
-import time
-import random
 import create_driver
+import logging
 
 
-# 팝업이 노출될 수 있어서 팝업이 노출되면 클릭을 하고, 노출되지 않으면 해당 부분 지나감.
+# 팝업에서 [X] 버튼 클릭
 def popup():
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/div/div/button[2]'))
         )
         element.click()
-        print("popup() - PASS")
+        result_message = "popup() - PASS"
     except Exception as e:
-        print("popup() - Fail")
+        result_message = "popup() - Fail"
+
+    print(result_message)
+    return result_message
 
 
 # Work 클릭
 def click_work():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.LINK_TEXT, 'Work'))
         )
         element.click()
+
+        if create_driver.driver.current_url == 'https://illuminarean.com/work':
+            result_message = "click_work() - PASS"
+        else:
+            result_message = "click_apply() - Fail"
     except Exception as e:
         print("Work 링크가 없습니다.")
 
-    if create_driver.driver.current_url == 'https://illuminarean.com/work':
-        print("click_work() - PASS")
-    else:
-        print("click_apply() - Fail")
+    print(result_message)
+    return result_message
 
 
 # [GOODVIBE WORKS 바로가기] 버튼 클릭
@@ -52,72 +53,91 @@ def click_goodvibe_works():
 
 # 드라이버의 탭을 새로운 탭으로 변경
 def change_tab():
-    create_driver.driver.switch_to.window(create_driver.driver.window_handles[1])
+    result_message = ""
+    try:
+        create_driver.driver.switch_to.window(create_driver.driver.window_handles[1])
 
-    if create_driver.driver.current_url == "https://works.goodvibe.kr/":
-        print("click_goodvibe_works(), change_tab() - PASS")
-    else:
-        print("click_goodvibe_works(), change_tab() - Fail", end=" ")
-        print(f"현재 탭은 {create_driver.driver.current_url} 입니다.")
+        if create_driver.driver.current_url == "https://works.goodvibe.kr/":
+            result_message = "click_goodvibe_works(), change_tab() - PASS"
+        else:
+            result_message = f"click_goodvibe_works(), change_tab() - Fail " \
+                             f"현재 탭은 {create_driver.driver.current_url} 입니다."
+    except Exception as e:
+        print("새로운 tab으로 변경되지 않았습니다.")
+
+    print(result_message)
+    return result_message
 
 
 # [무료 체험 신청하기] 버튼 클릭
 def click_apply():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//button[text()='무료 체험 신청']"))
         )
         element.click()
+        if create_driver.driver.find_element(By.XPATH,
+                                             "/html/body/div[5]/div/div/div/div/div/div/div/div[1]/h2").text == "서비스 이용신청":
+            result_message = "click_apply() - PASS"
+        else:
+            result_message = "click_apply() - Fail"
     except Exception as e:
         print("[무료 체험 신청하기] 버튼이 없습니다.")
 
-    if create_driver.driver.find_element(By.XPATH,
-                                         "/html/body/div[5]/div/div/div/div/div/div/div/div[1]/h2").text == "서비스 이용신청":
-        print("click_apply() - PASS")
-    else:
-        print("click_apply() - Fail")
+    print(result_message)
+    return result_message
 
 
 # 내용 입력 - 회사명
 def input_company_name():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.ID, 'companyName'))
         )
         element.clear()
         element.send_keys(f'테스트회사{create_driver.randomNum}')
+
+        if create_driver.driver.find_element(By.ID, 'companyName').get_attribute("value") == \
+                "테스트회사" + str(create_driver.randomNum):
+            result_message = "input_company_name() - PASS"
+        else:
+            result_message = f"input_company_name() - Fail 현재 입력된 값은 " \
+                             f"{create_driver.driver.find_element(By.ID, 'companyName').get_attribute('value')} 입니다."
     except Exception as e:
         print("내용 입력 - 회사명이 없습니다.")
 
-    if create_driver.driver.find_element(By.ID, 'companyName').get_attribute("value") == \
-            "테스트회사"+str(create_driver.randomNum):
-        print("input_company_name() - PASS")
-    else:
-        print("input_company_name() - Fail", end=" ")
-        print(f"현재 입력된 값은 {create_driver.driver.find_element(By.ID, 'companyName').get_attribute('value')} 입니다.")
+    print(result_message)
+    return result_message
 
 
 # 내용 입력 - 대표자명
 def input_ceo_name():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.ID, 'ceoName'))
         )
         element.clear()
         element.send_keys(f'테스트이름{create_driver.randomNum}')
+
+        if create_driver.driver.find_element(By.ID, 'ceoName').get_attribute("value") == \
+                "테스트이름" + str(create_driver.randomNum):
+            result_message = "input_ceo_name() - PASS"
+        else:
+            result_message = f"input_ceo_name() - Fail 현재 입력된 값은 " \
+                             f"{create_driver.driver.find_element(By.ID, 'ceoName').get_attribute('value')} 입니다."
     except Exception as e:
         print("내용 입력 - 대표자명이 없습니다.")
 
-    if create_driver.driver.find_element(By.ID, 'ceoName').get_attribute("value") == \
-            "테스트이름" + str(create_driver.randomNum):
-        print("input_ceo_name() - PASS")
-    else:
-        print("input_ceo_name() - Fail", end=" ")
-        print(f"현재 입력된 값은 {create_driver.driver.find_element(By.ID, 'ceoName').get_attribute('value')} 입니다.")
+    print(result_message)
+    return result_message
 
 
 # 내용 선택 - 사업자 유형(개인)
 def select_business_type():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.ID, 'businessType'))
@@ -128,21 +148,24 @@ def select_business_type():
 
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,"//*[text()='개인']"))
+            EC.presence_of_element_located((By.XPATH, "//*[text()='개인']"))
         )
         element.click()
 
         if create_driver.driver.find_element(By.XPATH, '//*[@id="businessType"]/div/div[1]/div').text == '개인':
-            print("select_business_type() - PASS")
+            result_message = "select_business_type() - PASS"
         else:
-            print("select_business_type() - Fail", end=" ")
-            print(f"선택된 값은 개인이 아닌 {element.text} 입니다.")
+            result_message = f"select_business_type() - Fail 선택된 값은 개인이 아닌 {element.text} 입니다."
     except Exception as e:
         print("내용 선택 - 사업자 유형에 '개인'이 없습니다.")
+
+    print(result_message)
+    return result_message
 
 
 # 내용 선택 - 직원수(21-50명)
 def select_scale():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.ID, 'scale'))
@@ -153,74 +176,91 @@ def select_scale():
 
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,"//*[text()='51-100 명']"))
+            EC.presence_of_element_located((By.XPATH, "//*[text()='51-100 명']"))
         )
         element.click()
 
         if create_driver.driver.find_element(By.XPATH, '//*[@id="scale"]/div/div[1]/div').text == '51-100 명':
-            print("select_scale() - PASS")
+            result_message = "select_scale() - PASS"
         else:
-            print("select_scale() - Fail", end=" ")
-            print(f"선택된 값은 '51-100 명'이 아닌 {element.text} 입니다.")
+            result_message = f"select_scale() - Fail 선택된 값은 '51-100 명'이 아닌 {element.text} 입니다."
     except Exception as e:
         print("내용 선택 - 직원수에 '51-100 명'이 없습니다.")
+
+    print(result_message)
+    return result_message
 
 
 # 내용 입력 - 담당자명
 def input_name():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.ID, 'name'))
         )
         element.clear()
         element.send_keys(f'테스트담당자명{create_driver.randomNum}')
+
+        if create_driver.driver.find_element(By.ID, 'name').get_attribute("value") == \
+                "테스트담당자명" + str(create_driver.randomNum):
+            result_message = "input_name() - PASS"
+        else:
+            result_message = "input_name() - Fail"
     except Exception as e:
         print("내용 입력 - 담당자명이 없습니다.")
 
-    if create_driver.driver.find_element(By.ID, 'name').get_attribute("value") == \
-            "테스트담당자명" + str(create_driver.randomNum):
-        print("input_name() - PASS")
-    else:
-        print("input_name() - Fail")
+    print(result_message)
+    return result_message
 
 
 # 내용 입력 - 이메일
 def input_email():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.ID, 'email'))
         )
         element.clear()
         element.send_keys(f'test{create_driver.randomNum}@test.com')
+
+        if create_driver.driver.find_element(By.ID, 'email').get_attribute("value") == \
+                "test" + str(create_driver.randomNum) + "@test.com":
+            result_message = "input_email() - PASS"
+        else:
+            result_message = "input_email() - Fail"
+
     except Exception as e:
         print("내용 입력 - 이메일이 없습니다.")
 
-    if create_driver.driver.find_element(By.ID, 'email').get_attribute("value") == \
-            "test" + str(create_driver.randomNum) + "@test.com":
-        print("input_email() - PASS")
-    else:
-        print("input_email() - Fail")
+    print(result_message)
+    return result_message
 
 
 # 내용 입력 - 휴대폰 번호
 def input_mobile():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.ID, 'mobile'))
         )
         element.clear()
-        element.send_keys('010-0000-0000') # 실제 번호가 있을 수 있어서 해당 부분은 randomNum을 사용하지 않음
+        element.send_keys('010-0000-0000')  # 실제 번호가 있을 수 있어서 해당 부분은 randomNum을 사용하지 않음
+
+        if create_driver.driver.find_element(By.ID, 'mobile').get_attribute("value") == '010-0000-0000':
+            result_message = "input_mobile() - PASS"
+        else:
+            result_message = "input_mobile() - Fail"
+
     except Exception as e:
         print("내용 입력 - 휴대폰 번호가 없습니다.")
 
-    if create_driver.driver.find_element(By.ID, 'mobile').get_attribute("value") == '010-0000-0000':
-        print("input_mobile() - PASS")
-    else:
-        print("input_mobile() - Fail")
+    print(result_message)
+    return result_message
 
 
 # 내용 선택 - 담당 업무
 def select_responsibility():
+    result_message = ""
     # 먼저 버튼 클릭
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
@@ -239,16 +279,19 @@ def select_responsibility():
         element.click()
 
         if element.text == '스타일리스트':
-            print("select_responsibility() - PASS")
+            result_message = "select_responsibility() - PASS"
         else:
-            print("select_responsibility() - Fail", end=" ")
-            print(f"선택된 값은 {element.text} 입니다.")
+            result_message = f"select_responsibility() - Fail 선택된 값은 {element.text} 입니다."
     except Exception as e:
         print("담당 업무명에 '스타일리스트'가 없습니다.")
+
+    print(result_message)
+    return result_message
 
 
 # 내용 검색 - 담당 업무
 def search_responsibility():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div/div/div/div/div/'
@@ -266,66 +309,84 @@ def search_responsibility():
         element.click()
 
         if element.text == '대표':
-            print("search_responsibility() - PASS")
+            result_message = "search_responsibility() - PASS"
         else:
-            print("search_responsibility() - Fail", end=" ")
-            print(f"선택된 값은 {element.text} 입니다.")
+            result_message = f"search_responsibility() - Fail 선택된 값은 {element.text} 입니다."
     except Exception as e:
         print("담당 업무명에 '대표'가 없습니다.")
+
+    print(result_message)
+    return result_message
 
 
 # 담당 업무명 [등록] 버튼 클릭
 def click_registration():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//button[text()='등록']"))
         )
         element.click()
+
+        if create_driver.driver.find_element(By.XPATH,
+                                             "/html/body/div[5]/div/div/div/div/div/div/div/"
+                                             "div[2]/dl[8]/dd/div/div[2]/ul/li/span").text == "스타일리스트":
+            result_message = "click_registration() - PASS"
+        else:
+            result_message = "click_registration() - Fail"
+
     except Exception as e:
         print("담당 업무명 [등록] 버튼이 없습니다.")
 
-    if create_driver.driver.find_element(By.XPATH,
-                                         "/html/body/div[5]/div/div/div/div/div/div/div/"
-                                         "div[2]/dl[8]/dd/div/div[2]/ul/li/span").text == "스타일리스트":
-        print("click_registration() - PASS")
-    else:
-        print("click_registration() - Fail")
+    print(result_message)
+    return result_message
 
 
 # 이용동의약관 체크박스 선택
 def click_agree_terms_of_use():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
-            EC.presence_of_element_located((By.ID,'agreeTermsOfUse'))
+            EC.presence_of_element_located((By.ID, 'agreeTermsOfUse'))
         )
         element.click()
+
+        if create_driver.driver.find_element(By.ID, 'agreeTermsOfUse').is_selected():
+            result_message = "click_agree_terms_of_use() - PASS"
+        else:
+            result_message = "click_agree_terms_of_use() - Fail"
+
     except Exception as e:
         print("약관 동의 이용동의약관 이 없습니다.")
 
-    if create_driver.driver.find_element(By.ID,'agreeTermsOfUse').is_selected():
-        print("click_agree_terms_of_use() - PASS")
-    else:
-        print("click_agree_terms_of_use() - Fail")
+    print(result_message)
+    return result_message
 
 
 # 개인정보동의약관 체크박스 선택
 def click_agree_privacy_statement():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
-            EC.presence_of_element_located((By.ID,'agreePrivacyStatement'))
+            EC.presence_of_element_located((By.ID, 'agreePrivacyStatement'))
         )
         element.click()
+
+        if create_driver.driver.find_element(By.ID, 'agreePrivacyStatement').is_selected():
+            result_message = "click_agree_privacy_statement() - PASS"
+        else:
+            result_message = "click_agree_privacy_statement() - Fail"
+
     except Exception as e:
         print("약관 동의 개인정보동의약관 동의 체크박스가 없습니다.")
 
-    if create_driver.driver.find_element(By.ID, 'agreePrivacyStatement').is_selected():
-        print("click_agree_privacy_statement() - PASS")
-    else:
-        print("click_agree_privacy_statement() - Fail")
+    print(result_message)
+    return result_message
 
 
 # [신청 취소] 버튼 클릭
 def click_unsubscribe_button():
+    result_message = ""
     try:
         element = WebDriverWait(create_driver.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/button'))
@@ -349,8 +410,11 @@ def click_unsubscribe_button():
             EC.presence_of_element_located((By.LINK_TEXT, '로그인'))
         )
         if element.text == "로그인":
-            print("click_unsubscribe_button() - PASS")
+            result_message = "click_unsubscribe_button() - PASS"
         else:
-            print("click_unsubscribe_button() - Fail")
+            result_message = "click_unsubscribe_button() - Fail"
     except Exception as e:
         print("현재 페이지에 로그인 버튼이 없습니다.")
+
+    print(result_message)
+    return result_message
--- test_exam.py
+++ test_exam.py
@@ -2,6 +2,8 @@ import unittest
 import time
 import exam
 import create_driver
+import logging
+import os
 
 
 class TestApplyService(unittest.TestCase):
@@ -11,6 +13,7 @@ class TestApplyService(unittest.TestCase):
         # driver에 url 넣음
         url = 'https://illuminarean.com/'
         create_driver.driver.get(url)
+
     @classmethod
     def setUp(self):
         time.sleep(1)
@@ -21,59 +24,85 @@ class TestApplyService(unittest.TestCase):
         create_driver.driver.quit()
 
     def test001(self):
-        exam.popup()
+        result_message = exam.popup()
+        logging.info(result_message)
 
     def test002(self):
-        exam.click_work()
+        result_message = exam.click_work()
+        logging.info(result_message)
 
     def test003(self):
         exam.click_goodvibe_works()
 
     def test004(self):
-        exam.change_tab()
+        result_message = exam.change_tab()
+        logging.info(result_message)
 
     def test005(self):
-        exam.click_apply()
+        result_message = exam.click_apply()
+        logging.info(result_message)
 
     def test006(self):
-        exam.input_company_name()
+        result_message = exam.input_company_name()
+        logging.info(result_message)
 
     def test007(self):
-        exam.input_ceo_name()
+        result_message = exam.input_ceo_name()
+        logging.info(result_message)
 
     def test008(self):
-        exam.select_business_type()
+        result_message = exam.select_business_type()
+        logging.info(result_message)
 
     def test009(self):
-        exam.select_scale()
+        result_message = exam.select_scale()
+        logging.info(result_message)
 
     def test010(self):
-        exam.input_name()
+        result_message = exam.input_name()
+        logging.info(result_message)
 
     def test011(self):
-        exam.input_email()
+        result_message = exam.input_email()
+        logging.info(result_message)
 
     def test012(self):
-        exam.input_mobile()
+        result_message = exam.input_mobile()
+        logging.info(result_message)
 
     def test013(self):
-        exam.select_responsibility()
+        result_message = exam.select_responsibility()
+        logging.info(result_message)
 
     def test014(self):
-        exam.search_responsibility()
+        result_message = exam.search_responsibility()
+        logging.info(result_message)
 
     def test015(self):
-        exam.click_registration()
+        result_message = exam.click_registration()
+        logging.info(result_message)
 
     def test016(self):
-        exam.click_agree_terms_of_use()
+        result_message = exam.click_agree_terms_of_use()
+        logging.info(result_message)
 
     def test017(self):
-        exam.click_agree_privacy_statement()
+        result_message = exam.click_agree_privacy_statement()
+        logging.info(result_message)
 
     def test018(self):
-        exam.click_unsubscribe_button()
+        result_message = exam.click_unsubscribe_button()
+        logging.info(result_message)
 
 
 if __name__ == '__main__':
+    # 로그 생성하기 위해서 작성한 부분(pyCharm에서 실행하지 않고, 터미널에서 python3 test_exam.py 로 실행 시 test_log.txt 파일 생성됨)
+    # os.getcwd()는 현재 작업 디렉토리를 얻기 위한 함수
+    log_file_path = os.path.join(os.getcwd(), 'test_log.txt')
+    logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    # 테스트 실행
     unittest.main()
+
+    # 로그 파일 위치 출력
+    print("로그 파일 위치:", log_file_path)
